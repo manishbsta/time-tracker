0b1a8a3f7309a7b3e7985670eb2653a22074bb4a
Manish Bista <manishbsta@users.noreply.github.com>
2020-09-24 23:33:39 +0545

    WelcomeActivity Design complete

---
+++ app/src/main/java/com/manishbista/smarttracker/screens/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.manishbista.smarttracker.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.manishbista.smarttracker.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
